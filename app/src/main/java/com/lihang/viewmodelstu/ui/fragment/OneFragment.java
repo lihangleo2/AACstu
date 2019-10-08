1935ac843aa60223a923452089a06a19142183c5
leo <lihangleo2@gmail.com>
2019-10-08 14:52:25 +0800

    AACstu

---
+++ app/src/main/java/com/lihang/viewmodelstu/ui/fragment/OneFragment.java
@@ -0,0 +1,50 @@
+package com.lihang.viewmodelstu.ui.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lihang.viewmodelstu.R;
+import com.lihang.viewmodelstu.databinding.FragmentOneBinding;
+import com.lihang.viewmodelstu.utils.ToastUtils;
+import com.lihang.viewmodelstu.viewmodel.MyViewModel;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+/**
+ * Created by leo
+ * on 2019/10/8.
+ */
+public class OneFragment extends Fragment {
+    private int type;
+    FragmentOneBinding binding;
+
+    public OneFragment(int type) {
+        this.type = type;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_one, container, false);
+        if (type == 0) {
+            binding.relative.setBackgroundColor(getResources().getColor(R.color.color_1));
+            binding.btnOne.setText("Fragment - One：打印当前ViewHolder");
+        } else {
+            binding.relative.setBackgroundColor(getResources().getColor(R.color.color_2));
+            binding.btnOne.setText("Fragment - Two：打印当前ViewHolder");
+        }
+        binding.btnOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToastUtils.showToast(ViewModelProviders.of(getActivity()).get(MyViewModel.class).hashCode() + "");
+            }
+        });
+        return binding.getRoot();
+    }
+}
