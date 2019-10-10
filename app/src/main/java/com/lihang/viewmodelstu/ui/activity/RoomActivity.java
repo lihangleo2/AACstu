906458e2e61023088c1278b604b9c30d2735a78c
leo <lihangleo2@gmail.com>
2019-10-10 09:29:02 +0800

    AACstu

---
+++ app/src/main/java/com/lihang/viewmodelstu/ui/activity/RoomActivity.java
@@ -0,0 +1,23 @@
+package com.lihang.viewmodelstu.ui.activity;
+
+import android.os.Bundle;
+
+import com.lihang.viewmodelstu.R;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by leo
+ * on 2019/10/10.
+ */
+public class RoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room);
+
+
+    }
+}
